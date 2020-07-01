43f454c31a64bbfca826dbca0716be6c762e260f
Top Nguyen <admin@topnguyen.com>
2020-07-01 16:24:35 +0700

    add validation

--- src/Cross/Goblin.Identity.Core/Validators/RegisterUserModelValidator.cs
+++ src/Cross/Goblin.Identity.Core/Validators/RegisterUserModelValidator.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentValidation;
 using Goblin.Identity.Share.Models;
 
@@ -7,6 +8,37 @@ namespace Goblin.Identity.Core.Validators
     {
         public RegisterUserModelValidator()
         {
+            RuleFor(x => x.AvatarUrl)
+                .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _) && uri.Length < 1000)
+                .When(x => !string.IsNullOrWhiteSpace(x.AvatarUrl));
+            
+            RuleFor(x => x.FullName)
+                .NotEmpty()
+                .MaximumLength(200);
+            
+            RuleFor(x => x.Bio)
+                .MaximumLength(500);
+
+            RuleFor(x => x.GithubId)
+                .MaximumLength(200);
+            
+            RuleFor(x => x.SkypeId)
+                .MaximumLength(200);
+            
+            RuleFor(x => x.FacebookId)
+                .MaximumLength(200);
+            
+            RuleFor(x => x.WebsiteUrl)
+                .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _) && uri.Length < 1000)
+                .When(x => !string.IsNullOrWhiteSpace(x.AvatarUrl));
+
+            RuleFor(x => x.CompanyName)
+                .MaximumLength(500);
+ 
+            RuleFor(x => x.CompanyUrl)
+                .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _) && uri.Length < 1000)
+                .When(x => !string.IsNullOrWhiteSpace(x.AvatarUrl));
+
             RuleFor(x => x.Email)
                 .NotEmpty()
                 .EmailAddress()
@@ -20,11 +52,7 @@ namespace Goblin.Identity.Core.Validators
             RuleFor(x => x.Password)
                 .NotEmpty()
                 .MinimumLength(4)
-                .MaximumLength(200);
-            
-            RuleFor(x => x.FullName)
-                .NotEmpty()
-                .MaximumLength(200);
+                .MaximumLength(200);          
         }
     }
 }
